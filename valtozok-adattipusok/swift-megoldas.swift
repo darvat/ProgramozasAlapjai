a95637197c3e4a83612dbfc5daa93ce6073df48f
Tamas Darvas <tamas@darvas.ch>
2020-05-12 20:30:01 +0200

    Create swift-megoldas.swift

---
+++ valtozok-adattipusok/swift-megoldas.swift
@@ -0,0 +1,12 @@
+var evekSzama = 46
+var leggyorsabbSzaz = 11.48
+var kedvencMotor = "Ducati"
+let legjobbAuto = "BMW"
+
+print(evekSzama)
+print(leggyorsabbSzaz)
+print(kedvencMotor)
+print(legjobbAuto)
+
+evekSzama = 44
+print(evekSzama)
