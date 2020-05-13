9e55dbafc1232ada3b799100f1b7abbf472dfa65
Tamas Darvas <tamas@darvas.ch>
2020-05-13 07:27:42 +0200

    Update python-megoldas.py

--- valtozok-adattipusok/python-megoldas.py
+++ valtozok-adattipusok/python-megoldas.py
@@ -1,7 +1,13 @@
+
 evekSzama = 46
 leggyorsabbSzaz = 11.48
 kedvencMotor = "Ducati"
 
 print(evekSzama)
 print(leggyorsabbSzaz)
+print(kedvencMotor)
+
+print(type(evekSzama))
+
+kedvencMotor = "Cagiva"
 print(kedvencMotor)
