66be4fbe0248401f38f086079dd6389011a58762
Tamas Darvas <tamas@darvas.ch>
2020-05-17 20:24:45 +0200

    Update megoldas.js

--- operatorok/megoldas.js
+++ operatorok/megoldas.js
@@ -1,5 +1,5 @@
 let valt1 = 35
 let valt2 = 57
-let diff = valt1 -valt2
+let diff = valt1 - valt2
 ++valt1
-console.log(valt1 < valt2 ? "kisebb" : "nagyobb")
+console.log(valt1 < valt2 ? "kisebb" : "nem kisebb")
