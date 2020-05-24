ea34f52bed84086a96d92b6040f19c509758f0cc
Tamas Darvas <tamas@darvas.ch>
2020-05-24 20:43:28 +0200

    Create source.py

---
+++ elagazasok-if-szerkezet/source.py
@@ -0,0 +1,21 @@
+szepVagyok = True
+
+if szepVagyok:
+  print("Tudom, hogy szep vagy")
+  print("de lehetsz meg szebb")
+else:
+  print("csunya vagy")
+  print("de lehetsz meg szep")
+
+felesegekSzama = 1
+
+if felesegekSzama < 1:
+  print("meg nem ert el a vegzet")
+elif felesegekSzama == 1:
+  print("ismered a dorgest")  
+elif felesegekSzama == 2:
+  print("ketto jobb mint egy?")  
+else:
+  print("poligamia rulez")  
+
+print("ez itt az if szerkezet utan van")  
