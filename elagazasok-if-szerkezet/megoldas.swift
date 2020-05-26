0d9fd7bcf3d84aa14f33612fc73d07686ae71ffb
Tamas Darvas <tamas@darvas.ch>
2020-05-26 11:30:34 +0200

    Create megoldas.swift

---
+++ elagazasok-if-szerkezet/megoldas.swift
@@ -0,0 +1,10 @@
+let ideiEv = 2020
+let szuletesiEvem = 1974
+
+if ideiEv - szuletesiEvem < 18 {
+  print("kiskoru")
+} else if ideiEv - szuletesiEvem >= 18 && ideiEv - szuletesiEvem < 65 {
+  print("nagykoru")
+} else {
+  print("nyugdijas")
+}
