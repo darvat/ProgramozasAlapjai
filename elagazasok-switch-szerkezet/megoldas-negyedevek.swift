dffaaf54a54453ccface668d6dd707d46eb2f726
Tamas Darvas <tamas@darvas.ch>
2020-05-29 11:59:18 +0200

    Update megoldas-negyedevek.swift

--- elagazasok-switch-szerkezet/megoldas-negyedevek.swift
+++ elagazasok-switch-szerkezet/megoldas-negyedevek.swift
@@ -2,7 +2,7 @@ let honapNev = "Október"
 var negyedEv = 0
  
 switch (honapNev) {
- case "Január" ,"Február","Március":
+ case "Január", "Február","Március":
    negyedEv = 1
  case "Április", "Május", "Június":
    negyedEv = 2
